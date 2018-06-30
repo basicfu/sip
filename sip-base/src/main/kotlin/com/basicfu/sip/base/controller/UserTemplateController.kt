6c9e91cac584839439dbca76632341d756c5d4a0
basicfu <basicfu@gmail.com>
2018-06-30 20:22:22 +0800

    sip-base add controller crud

---
+++ sip-base/src/main/kotlin/com/basicfu/sip/base/controller/UserTemplateController.kt
@@ -0,0 +1,38 @@
+package com.basicfu.sip.base.controller
+
+import com.basicfu.sip.base.model.vo.UserTemplateVo
+import com.basicfu.sip.base.service.UserTemplateService
+import com.basicfu.sip.core.model.Result
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.*
+
+/**
+ * @author basicfu
+ * @date 2018/6/30
+ */
+@RestController
+@RequestMapping("/user-template")
+class UserTemplateController {
+    @Autowired
+    lateinit var userTemplateService: UserTemplateService
+
+    @GetMapping("/all")
+    fun all(vo: UserTemplateVo): Result<Any> {
+        return Result(userTemplateService.all(vo))
+    }
+
+    @PostMapping("/insert")
+    fun insert(vo: UserTemplateVo): Result<Any> {
+        return Result(userTemplateService.insert(vo))
+    }
+
+    @PostMapping("/update")
+    fun update(vo: UserTemplateVo): Result<Any> {
+        return Result(userTemplateService.update(vo))
+    }
+
+    @DeleteMapping("/delete")
+    fun delete(ids: List<Long>): Result<Any> {
+        return Result(userTemplateService.delete(ids))
+    }
+}
