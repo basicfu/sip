c68547572c7dbb9ea71a51df3a4d889b7fd2ea3a
basicfu <basicfu@gmail.com>
2018-07-12 15:55:17 +0800

    client controller add method get service all url

--- sip-client/src/main/kotlin/com/basicfu/sip/client/controller/UtilController.kt
+++ sip-client/src/main/kotlin/com/basicfu/sip/client/controller/UtilController.kt
@@ -1,7 +1,6 @@
 package com.basicfu.sip.client.controller
 
-import com.alibaba.fastjson.JSON
-import com.alibaba.fastjson.JSONObject
+import com.alibaba.fastjson.JSONArray
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.core.LocalVariableTableParameterNameDiscoverer
 import org.springframework.web.bind.annotation.GetMapping
@@ -18,9 +17,9 @@ class UtilController {
     @Autowired
     lateinit var requestMappingHandlerMapping: RequestMappingHandlerMapping
 
-    @GetMapping("/client/url","/test/tttt")
-    fun getUrl() {
-        val urlList = ArrayList<HashMap<String, Any>>()
+    @GetMapping("/sip/client/url")
+    fun getServiceAllUrl(): JSONArray {
+        val result = JSONArray()
         val map = requestMappingHandlerMapping.handlerMethods
         for (m in map.entries) {
             val hashMap = HashMap<String, Any>()
@@ -28,28 +27,15 @@ class UtilController {
             val handlerMethod = m.value
             val methodName = handlerMethod.method.name
             hashMap["url"] = info.patternsCondition.patterns
-            hashMap["type"] = info.methodsCondition.methods
+            hashMap["requestMethod"] = info.methodsCondition.methods
             hashMap["className"] = handlerMethod.method.declaringClass.name
-            hashMap["method"] = methodName
+            hashMap["methodName"] = methodName
             hashMap["returnClassName"] = handlerMethod.method.returnType.name
-            hashMap["parameter"] = handlerMethod.method.parameterTypes
-            urlList.add(hashMap)
-            println(JSON.toJSONString(hashMap))
-            val parameterNames =
-                LocalVariableTableParameterNameDiscoverer().getParameterNames(handlerMethod.method)
-            val jsonObject = JSONObject()
-            if (handlerMethod.method.parameterTypes != null) {
-                val parameterTypes = handlerMethod.method.parameterTypes
-                for (parameterType in parameterTypes) {
-                    if (parameterType.name.endsWith("Vo")) {
-                        val fields = parameterType.declaredFields
-                        for (field in fields) {
-                            jsonObject[field.name] = ""
-                        }
-                    }
-                }
-            }
-//            System.out.println(jsonObject.toJSONString())
+            hashMap["parameterType"] = handlerMethod.method.parameterTypes
+            hashMap["parameterName"] =
+                    LocalVariableTableParameterNameDiscoverer().getParameterNames(handlerMethod.method)
+            result.add(hashMap)
         }
+        return result
     }
 }
