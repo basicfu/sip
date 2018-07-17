7b0c929f8cd5a04b1ec4b3a678c795d3681f4b73
basicfu <basicfu@gmail.com>
2018-07-17 15:04:25 +0800

    fix two dictDto no same

--- sip-client/src/main/kotlin/com/basicfu/sip/client/model/DictDto.kt
+++ sip-client/src/main/kotlin/com/basicfu/sip/client/model/DictDto.kt
@@ -4,10 +4,11 @@ class DictDto {
     var id: Long? = null
     var name: String? = null
     var value: String? = null
+    var description: String? = null
     var lft: Long? = null
     var rgt: Long? = null
     var lvl: Int? = null
-    var pid: Long? = null
-    var fixed:Int?=null
+    var sort:Int?=null
+    var fixed:Boolean?=null
     var children:List<DictDto>?=null
 }
