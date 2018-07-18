7d7c4e9e3f4529297bfbb8b70a21024e9338d466
tianhu <18910751785@163.com>
2018-07-18 21:06:27 +0800

    add Dockerfile

--- sip-notify/src/main/kotlin/com/basicfu/sip/notify/mapper/MailSenderMapper.java
+++
@@ -1,8 +0,0 @@
-package com.basicfu.sip.notify.mapper;
-
-
-import com.basicfu.sip.core.common.mapper.CustomMapper;
-import com.basicfu.sip.notify.model.po.MailSender;
-
-public interface MailSenderMapper extends CustomMapper<MailSender> {
-}
