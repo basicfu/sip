97c4847e3fbfc980639c64968c678317b76cd186
tianhu <18910751785@163.com>
2018-07-24 10:00:16 +0800

    Remove redundant references and rename MailUtil method

--- sip-notify/src/main/kotlin/com/basicfu/sip/notify/controller/MailTemplateController.kt
+++ sip-notify/src/main/kotlin/com/basicfu/sip/notify/controller/MailTemplateController.kt
@@ -1,12 +1,14 @@
 package com.basicfu.sip.notify.controller
 
 import com.alibaba.fastjson.JSON
-import com.basicfu.sip.client.util.MailUtil
 import com.basicfu.sip.core.model.Result
 import com.basicfu.sip.notify.model.vo.SendMailVo
 import com.basicfu.sip.notify.service.MailTemplateService
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
 /**
  * @author tianhu
