77d62a440c23c06e6af8f3435603606f895cfd47
basicfu <basicfu@gmail.com>
2018-07-31 17:55:07 +0800

    fix getway select menu left join change to inner join

--- sip-getway/src/main/kotlin/com/basicfu/sip/getway/mapper/Mapper.kt
+++ sip-getway/src/main/kotlin/com/basicfu/sip/getway/mapper/Mapper.kt
@@ -21,12 +21,12 @@ interface Mapper {
     @Select("SELECT id,app_id as appId,name,path,server_id as serverId,url,strip_prefix as stripPrefix,retryable,sensitive_headers as sensitiveHeaders from service")
     fun selectService(): List<Service>
 
-    @Select("SELECT m.id as id,m.app_id as appId,m.pid as pid,m.name as name,m.path as path,m.sort as sort,m.icon as icon,m.type as type,m.display as display FROM role r LEFT JOIN role_menu rm on r.id=rm.role_id LEFT JOIN menu m on rm.menu_id=m.id WHERE r.code=#{code}")
+    @Select("SELECT m.id as id,m.app_id as appId,m.pid as pid,m.name as name,m.path as path,m.sort as sort,m.icon as icon,m.type as type,m.display as display FROM role r INNER JOIN role_menu rm on r.id=rm.role_id INNER JOIN menu m on rm.menu_id=m.id WHERE r.code=#{code}")
     fun selectMenuByRoleCode(@Param("code") code:String): List<MenuDto>
 
-    @Select("SELECT resource.service_id as serviceId,resource.url as url,resource.method as method FROM role r LEFT JOIN role_permission rp on r.id=rp.role_id INNER JOIN permission_resource pr ON rp.permission_id=pr.permission_id LEFT JOIN resource on resource.id=pr.resource_id where r.code=#{code}")
+    @Select("SELECT resource.service_id as serviceId,resource.url as url,resource.method as method FROM role r INNER JOIN role_permission rp on r.id=rp.role_id INNER JOIN permission_resource pr ON rp.permission_id=pr.permission_id INNER JOIN resource on resource.id=pr.resource_id where r.code=#{code}")
     fun selectMenuResourceIdByRoleCode(@Param("code") code: String): List<Resource>
 
-    @Select("SELECT resource.service_id as serviceId,resource.url as url,resource.method as method FROM role r LEFT JOIN role_menu rm on r.id=rm.role_id INNER JOIN menu_resource mr ON rm.menu_id=mr.menu_id LEFT JOIN resource on resource.id=mr.resource_id where r.code=#{code}")
+    @Select("SELECT resource.service_id as serviceId,resource.url as url,resource.method as method FROM role r INNER JOIN role_menu rm on r.id=rm.role_id INNER JOIN menu_resource mr ON rm.menu_id=mr.menu_id INNER JOIN resource on resource.id=mr.resource_id where r.code=#{code}")
     fun selectPermissionResourceIdByRoleCode(@Param("code") code: String): List<Resource>
 }
