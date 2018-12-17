8ebffdc879b7c168995df7f6b57c712eb224a053
basicfu <basicfu@gmail.com>
2018-12-17 18:38:30 +0800

    fix: cat double quote escape

--- sip-tools/src/main/kotlin/com/basicfu/sip/tools/util/HelmUtil.kt
+++ sip-tools/src/main/kotlin/com/basicfu/sip/tools/util/HelmUtil.kt
@@ -1,26 +1,30 @@
 package com.basicfu.sip.tools.util
 
-import org.apache.commons.lang3.StringUtils
-
 /**
  * @author basicfu
  * @date 2018/8/18
  */
-object HelmUtil{
-    fun exists(name:String):Boolean{
+object HelmUtil {
+    fun exists(name: String): Boolean {
         val result = SSHUtil.execShell("helm status $name")
         return !result.contains("release: \"$name\" not found")
     }
-    fun install(name:String,namespace:String,values:String,param:String):String{
+
+    fun install(name: String, namespace: String, values: String, param: String): String {
         return SSHUtil.execShell(
-            "cat <<< '" + values +
-                    "' > values.yaml &&\n" +
-                    "helm install example-0.0.1.tgz -f values.yaml -n $name --namespace $namespace $param"
+            "tee values.yaml << EOF\n"
+                    + values +
+                    "EOF\n" +
+                    "helm install example-0.0.1.tgz -f values.yaml -n $name --namespace $namespace $param && rm -f values.yaml"
         )
     }
-    fun update(name:String,values:String,param:String):String{
-        return SSHUtil.execShell("cat <<< '" + values +
-                "' > values.yaml &&\n" +
-                "helm upgrade $name example-0.0.1.tgz -f values.yaml $param")
+
+    fun update(name: String, values: String, param: String): String {
+        return SSHUtil.execShell(
+            "tee values.yaml << EOF\n"
+                    + values +
+                    "EOF\n" +
+                    "helm upgrade $name example-0.0.1.tgz -f values.yaml $param && rm -f values.yaml"
+        )
     }
 }
