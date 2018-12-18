4e7c09d691de2d5d1b6b70a49a288b3f411270ac
basicfu <basicfu@gmail.com>
2018-12-18 18:14:52 +0800

    sql: add init sql

--- sip-docs/sql/sip-base.sql
+++ sip-docs/sql/sip-base.sql
@@ -90,7 +90,11 @@ CREATE TABLE user_template (
   unique key (app_id,name)
 )comment '用户模板' engine=InnoDB;
 
-
-INSERT INTO app (id,name, code, cdate, udate) VALUES (1,'SIP', 'sip', 1531389485, 1531389485);
-INSERT INTO user (id,app_id, username, nickname, content, cdate, udate, cuid, type, status) VALUES (1,1, 'test', 'test', '{}', 1533019466, 1539054488, 0, 'SYSTEM_SUPER_ADMIN', 0);
-INSERT INTO user_auth (id,app_id,uid, type, username, password, cdate, udate, ldate) VALUES (1,1,1, 0, 'test', '$2a$10$3d7ykD7OxOTglE6DcLdhWerSpViDhIAyz6CylkBg.QkRlTgtduQCa', 1533019466, 1539002195, 1538995227);
+INSERT INTO user (id,app_id, username, nickname, content, cdate, udate, cuid, type, status) VALUES (1,1, 'test', 'test', '{}', unix_timestamp(), unix_timestamp(), 0, 'SYSTEM_SUPER_ADMIN', 0);
+INSERT INTO user_auth (id,app_id,uid, type, username, password, cdate, udate, ldate) VALUES (1,1,1, 0, 'test', '$2a$10$3d7ykD7OxOTglE6DcLdhWerSpViDhIAyz6CylkBg.QkRlTgtduQCa', unix_timestamp(), unix_timestamp(), unix_timestamp());
+INSERT INTO app (id,name, code, cdate, udate) VALUES (1,'SIP', 'sip', unix_timestamp(), unix_timestamp());
+INSERT INTO app_service (id,app_id, name, path, server_id, url, strip_prefix, retryable, sensitive_headers, cdate, udate) VALUES (1,1, 'sip-base', '/base/**', 'sip-base', '', 1, 1, null, unix_timestamp(), unix_timestamp());
+INSERT INTO app_service (id,app_id, name, path, server_id, url, strip_prefix, retryable, sensitive_headers, cdate, udate) VALUES (2,1, 'sip-dict', '/dict/**', 'sip-dict', '', 1, 1, null, unix_timestamp(), unix_timestamp());
+INSERT INTO app_service (id,app_id, name, path, server_id, url, strip_prefix, retryable, sensitive_headers, cdate, udate) VALUES (3,1, 'sip-permission', '/permission/**', 'sip-permission', '', 1, 1, null, unix_timestamp(), unix_timestamp());
+INSERT INTO app_service (id,app_id, name, path, server_id, url, strip_prefix, retryable, sensitive_headers, cdate, udate) VALUES (4,1, 'sip-notify', '/notify/**', 'sip-notify', '', 1, 1, null, unix_timestamp(), unix_timestamp());
+INSERT INTO app_service (id,app_id, name, path, server_id, url, strip_prefix, retryable, sensitive_headers, cdate, udate) VALUES (5,1, 'sip-tools', '/tools/**', 'sip-tools', '', 1, 1, null, unix_timestamp(), unix_timestamp());
