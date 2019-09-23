2c550719f40e0411d466ee5c3759015e953d16f3
basicfu <basicfu@gmail.com>
2019-09-23 10:25:48 +0800

    feat: sms result

--- sip-base/src/main/kotlin/com/basicfu/sip/base/util/SmsUtil.kt
+++ sip-base/src/main/kotlin/com/basicfu/sip/base/util/SmsUtil.kt
@@ -1,5 +1,6 @@
 package com.basicfu.sip.base.util
 
+import com.alibaba.fastjson.JSON
 import com.aliyuncs.CommonRequest
 import com.aliyuncs.DefaultAcsClient
 import com.aliyuncs.exceptions.ClientException
@@ -8,6 +9,7 @@ import com.aliyuncs.http.MethodType
 import com.aliyuncs.profile.DefaultProfile
 import com.basicfu.sip.base.common.config.AliyunConfig
 import com.basicfu.sip.base.common.constant.Constant
+import com.basicfu.sip.core.common.exception.CustomException
 import com.basicfu.sip.core.util.RedisUtil
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
@@ -34,10 +36,16 @@ object SmsUtil {
         request.putQueryParameter("TemplateParam", "{'code':\"$code\"}")
         try {
             val response = client.getCommonResponse(request)
-            println(response.data)
-//            when(""){
-//
-//            }
+            val json=JSON.parseObject(response.data)
+            when(json.getString("Code")){
+                "OK"->{}
+                "isv.BUSINESS_LIMIT_CONTROL"->throw CustomException("发送频率频繁，请稍后重试")
+                "isv.BLACK_KEY_CONTROL_LIMIT"->throw CustomException("黑名单管控")
+                "isv.MOBILE_NUMBER_ILLEGAL"->throw CustomException("非法手机号")
+                "isv.SYSTEM_ERROR"->throw CustomException("系统错误，请稍后重试")
+                "isv.ACCOUNT_ABNORMAL","isv.AMOUNT_NOT_ENOUGH","isv.ACCOUNT_NOT_EXISTS"->throw CustomException("发送失败，请联系管理员")
+                else->throw CustomException("发送失败，请联系管理员")
+            }
         } catch (e: ServerException) {
             e.printStackTrace()
         } catch (e: ClientException) {
